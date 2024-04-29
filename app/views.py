acaa8d18672ec5c4c55f09b66df62540cdb6a566
mukkanti <mukkantisureshkumar777@gmail.com>
2024-04-29 17:15:21 +0530

    second commit

--- app/views.py
+++ app/views.py
@@ -29,22 +29,26 @@ def insert_webpage(request):
         return HttpResponse('Given Topic is Not present in My Parent Table')
 
 
-def insert_accessrecord(request):    
+def insert_accessrecord(request): 
+    tn=input('enter topic_name:')
+    to=Topic.objects.get_or_create(topic_name=tn)[0]
+    to.save()
+
+    
     n=input('enter name:')
+    u=input('enter url:')
+    e=input('enter email:')
+    na=Webpage.objects.get_or_create(topic_name=to,name=n,url=u,email=e)[0]
+    na.save()
+    
     d=input('enter date:')
     a=input('enter author:')
-    WO=Webpage.objects.all()
-    if WO:
-        ac=AccessRecord.objects.get_or_create(name=WO,date=d,author=a)[0]
-        ac.save()
-        return HttpResponse('accessrecord is created')
-    else:
-        return HttpResponse('Given Webpage is not present in my parent table ')
+    ac=AccessRecord.objects.get_or_create(name=na,date=d,author=a)[0]
+    ac.save()
+    return HttpResponse('accessrecord is created')
     
 
 
 
 
 
-
-def 
